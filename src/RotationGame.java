c8fcee65d0d6d4d3fe881b852746e1089ad0ef31
Sugan Krishna <suganplay039@gmail.com>
2022-05-10 07:11:01 +0530

    rotationgame problem completed

--- src/RotationGame.java
+++ src/RotationGame.java
@@ -19,11 +19,19 @@ public class RotationGame {
         } else {
             System.out.println("Need to rotate array - " + rotate);
         }*/
-        List<Integer> newArray1 = aArray.subList(0, B);
+        /*List<Integer> newArray1 = aArray.subList(0, B);
         System.out.println(newArray1);
         List<Integer> newArray2 = aArray.subList(aLength - B, aLength);
         System.out.println(newArray2);
         newArray2.addAll(newArray1);
-        System.out.println(newArray2);
+        System.out.println(newArray2);*/
+        for(int i = 0; i < B; i++) {
+            int last = aArray.get(aLength - 1);
+            for (int j = aLength - 1; j > 0; j--) {
+                aArray.set(j, aArray.get(j - 1));
+            }
+            aArray.set(0, last);
+        }
+        System.out.println(aArray);
     }
 }
